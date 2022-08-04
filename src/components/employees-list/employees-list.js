4e5852b425688f20d7eef5aed7e766687990af67
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-05 01:03:59 +0700

    update EmployessList

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import EmployeesItem from '../employees-item/employees-item';
 
-const EmployeesList = ({ data }) => {
+const EmployeesList = ({ data, onDelete, onToggleProp, changeStateEdit }) => {
   const elements = data.map((item) => {
     const { id, ...itemProps } = item;
-    return <EmployeesItem key={id} {...itemProps} />;
+    return (
+      <EmployeesItem
+        key={id}
+        {...itemProps}
+        onDelete={() => onDelete(id)}
+        onToggleProp={(e) =>
+          onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))
+        }
+        changeStateEdit={() => changeStateEdit(id)}
+      />
+    );
   });
   return <ul className='list-group'>{elements}</ul>;
 };
